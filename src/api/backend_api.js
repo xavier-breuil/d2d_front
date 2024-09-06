99612eabe16b21df4d4bfe7c0f0ec73cc06d9425
xavier <breuilxavier@gmail.com>
2024-09-06 12:06:10 +0200

    dev: parameter week task request to filter data on backend.

--- src/api/backend_api.js
+++ src/api/backend_api.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
+const backendBaseUrl = 'http://127.0.0.1:8000/';
+const backendUrls = {
+    weekTask:'week_task/',
+    datedTask: 'dated_task/'
+};
+
 export function getWeekTask(weekNum) {
-    return axios.get('http://127.0.0.1:8000/week_task/')
+    return axios.get(`${backendBaseUrl}${backendUrls.weekTask}?week_number=${weekNum}`)
         .then(response => {
             console.log(response);
         })
