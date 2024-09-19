33f33c1850c087db5ba19b784d4a177012f4ad18
xavier <breuilxavier@gmail.com>
2024-09-19 20:42:45 +0200

    dev: Display days and filter dated tasks againts days.

--- src/components/weekly.js
+++ src/components/weekly.js
@@ -1,8 +1,12 @@
 import { useState, useEffect } from 'react';
 
 import moment from 'moment';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import {getWeekTask} from '../api/backend_api';
+import {weekDays} from '../const/constants';
 
 function Weekly() {
     // define state
@@ -13,24 +17,55 @@ function Weekly() {
         getWeekTask(moment().isoWeek())
             .then(tasks => {
                 setWeekTasks(tasks.weeklyTask);
-                setDatedTasks(tasks.datedTask);
-            });
+                const datedTasks = tasks.datedTask;
+                datedTasks.map(task => {
+                    task.dayOfWeek = moment(task.date).isoWeekday()
+                })
+                setDatedTasks(datedTasks);
+            })
     },
 
         // no dependancies
         []
     )
     return (
-        <div>
-            <div>tasks for week {moment().isoWeek()}</div>
-            {weekTasks.map(wt => {
-                return <div key={wt.id}>{wt.name}</div>
-            })}
-            <div>dated tasks for week {moment().isoWeek()}</div>
-            {datedTasks.map(dt => {
-                return <div key={dt.id}>{dt.name}</div>
-            })}
-        </div>
+        <Container>
+            <Row>
+                <Col>semaine précédente</Col>
+                <Col>semaine {moment().isoWeek()}</Col>
+                <Col>semaine suivante</Col>
+            </Row>
+            <Row>
+                <Col>
+                    {weekDays.map(day => {
+                        return <div key={day.weekdayNumber}>
+                            <Row key={day.weekdayNumber} className="fw-bold mt-3">
+                                {day.name}
+                            </Row>
+                            {datedTasks.filter(
+                                t => t.dayOfWeek == day.weekdayNumber
+                                ).map(
+                                    t=> {
+                                        return (
+                                            <Row key={t.id} className="ps-3">
+                                                {t.name}
+                                            </Row>
+                                        )
+                                    }
+                                )
+                            }
+                        </div>
+                    })}
+                </Col>
+                <Col>
+                    {weekTasks.map(wt => {
+                        return (
+                            <div key={wt.id}>{wt.name}</div>
+                        )
+                    })}
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
---
+++ src/const/constants.js
@@ -0,0 +1,30 @@
+export const weekDays = [
+    {
+        weekdayNumber: 1,
+        name: 'lundi'
+    },
+    {
+        weekdayNumber: 2,
+        name: 'mardi'
+    },
+    {
+        weekdayNumber: 3,
+        name: 'mercredi'
+    },
+    {
+        weekdayNumber: 4,
+        name: 'jeudi'
+    },
+    {
+        weekdayNumber: 5,
+        name: 'vendredi'
+    },
+    {
+        weekdayNumber: 6,
+        name: 'samedi'
+    },
+    {
+        weekdayNumber: 7,
+        name: 'dimanche'
+    }
+]
