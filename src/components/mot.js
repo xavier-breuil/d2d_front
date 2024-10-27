f0f0722f2207c4eb04337fc56dc2551d7a8bbae1
xavier <breuilxavier@gmail.com>
2024-10-27 07:02:36 +0100

    dev: Add possibility to select a mot.

--- src/components/mot.js
+++ src/components/mot.js
@@ -1,9 +1,15 @@
 import { useState, useEffect } from 'react';
 
+import ListGroup from 'react-bootstrap/ListGroup';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Tab from 'react-bootstrap/Tab';
+
 import {getMots} from '../api/backend_api'
 
 const Mot = () => {
     const [motList, setMotList] = useState([]);
+    const [actMotId, setMotId] = useState('');
 
     useEffect(() => {
         getMots()
@@ -15,11 +21,27 @@ const Mot = () => {
     []
     )
 
-    return <div>
-        {motList.map(mot => {
-            return <div>{mot.name}</div>
-        })}
-    </div>
+    return (
+        <Tab.Container>
+            <Row>
+                <Col sm={3}>
+                    <ListGroup>
+                        {motList.map(mot => {
+                            return <ListGroup.Item
+                                action
+                                key={'item_' + mot.id}
+                                onClick={() => setMotId(mot.id)}>{mot.name}</ListGroup.Item>
+                        })}
+                    </ListGroup>
+                </Col>
+                <Col sm={9}>
+                    <Tab.Content>
+                        {actMotId}
+                    </Tab.Content>
+                </Col>
+            </Row>
+        </Tab.Container>
+    )
 }
 
 export default Mot;
