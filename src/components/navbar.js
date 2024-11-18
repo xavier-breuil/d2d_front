40ffcab08cbe917ee36de4aec9bb290b1e240a0a
xavier <breuilxavier@gmail.com>
2024-11-18 07:06:23 +0100

    dev: restyle navbar and make it sticky to top.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,15 +3,18 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 function NavBar() {
+
+const linkFormat = {color: 'white', fontSize: '1.3em'};
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" sticky="top" bg="primary">
       <Container>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-          <Nav.Link href="/">semainier</Nav.Link>
-          <Nav.Link href="/task">ajouter tâche</Nav.Link>
-          <Nav.Link href="/mot">tâche récurrente</Nav.Link>
+          <Nav.Link href="/" style={linkFormat}>semainier</Nav.Link>
+          <Nav.Link href="/task" style={linkFormat}>ajouter une tâche</Nav.Link>
+          <Nav.Link href="/mot" style={linkFormat}>tâches récurrentes</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
