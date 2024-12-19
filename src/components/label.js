f517b06e06f4ee8e6904878eb56f7a0c2b4908b2
xavier <breuilxavier@gmail.com>
2024-12-19 06:46:36 +0100

    dev: Add possibility to add or remove label form label interface

--- src/components/label.js
+++ src/components/label.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react';
 
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import Alert from 'react-bootstrap/Alert';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
 
 import {getLabels} from '../api/backend_api';
 
@@ -15,7 +16,12 @@ const Label = () => {
 
     useEffect(() => {
         getLabels()
-            .then(setLabels)
+            .then(labelList => {
+                labelList.forEach((label, index) => {
+                    labelList[index] = {...label, show: true}
+                });
+                setLabels(labelList);
+            })
             .catch(error => {
                 console.log(error);
                 displayAlert(true, 'danger', 'Erreur de récupération des étiquettes');
@@ -30,18 +36,56 @@ const Label = () => {
         window.scrollTo(0,0);
     }
 
+    const nameChanged = (event, label, index) => {
+        const newLabels = labels.map((lab, ind) => {
+            if (ind === index) {
+                return {name: event.target.value, id: label.id, show: label.show};
+            } else {
+                return lab;
+            }
+        })
+        setLabels(newLabels);
+    }
+
+    const addLabel = event => {
+        const newLabels = labels.map(lab => lab);
+        newLabels.push({name: '', show: true});
+        setLabels(newLabels);
+    }
+
+    const deleteLabel = (event, index) => {
+        const newLabels = labels.map((label, ind) => {
+            if (ind === index) {
+                return {...label, show: false};
+            } else {
+                return label;
+            }
+        })
+        setLabels(newLabels);
+    }
+
     return (
         <Container fluid className="px-0">
             {showAlert &&
                 <Alert variant={alertType} onClose={() => setShowAlert(false)} dismissible>{alertMessage}</Alert>
             }
-            {labels.map(label => {
+            {labels.map((label, index) => {
                 return (
-                    <Row key={'label-row-' + label.id}>
-                        <Col>{label.name}</Col>
-                    </Row>
-                )
+                    (label.show && <Stack direction="horizontal"
+                        key={'label-row-' + index}
+                        className="ms-4 mt-4"
+                        gap={3}>
+                        <Button variant="outline-danger" onClick={event => deleteLabel(event, index)}><i className="bi bi-trash" /></Button>
+                        <Form.Group className="col-lg-4 text-start">
+                            <Form.Control onChange={event => nameChanged(event, label, index)} value={label.name} />
+                        </Form.Group>
+                    </Stack>
+                ))
             })}
+            <Stack direction="horizontal" gap={4} className="ms-4 mt-4">
+                    <Button onClick={addLabel}>Ajouter</Button>
+                    <Button>Sauvegarder</Button>
+            </Stack>
       </Container>
     )
 }
