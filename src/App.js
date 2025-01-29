fde25286d867c8729a6b5031b57ec958850e4c34
xavier <breuilxavier@gmail.com>
2025-01-29 06:20:35 +0100

    dev: Add button for google oauth.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/navbar.js';
 
 import { RouterProvider } from 'react-router-dom';
+import { GoogleLogin } from '@react-oauth/google';
 
 import router from './components/router';
 
 function App() {
+
+  const responseMessage = (response) => {
+    console.log(response);
+  };
+
+  const errorMessage = (error) => {
+      console.log(error);
+  };
+
   return (
     <div className="App">
       <NavBar />
       <RouterProvider router={router}>
       </RouterProvider>
+      <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
     </div>
   );
 }
